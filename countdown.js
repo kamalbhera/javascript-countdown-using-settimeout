a11ddf70ed5f3c601cbc993ea723ac6fa7becf39
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-02-10 00:37:57 +0530

    first commit

---
+++ countdown.js
@@ -0,0 +1,46 @@
+const element = document.querySelector(".countdown")
+
+setTimeout(()=>{
+   element.innerText = 10;
+// },1000)
+setTimeout(()=>{
+  element.innerText = 9;
+// },2000)
+setTimeout(()=>{
+  element.innerText = 8;
+// },3000)
+setTimeout(()=>{
+  element.innerText = 7;
+// },4000)
+setTimeout(()=>{
+  element.innerText = 6;
+// },5000)
+setTimeout(()=>{
+  element.innerText = 5;
+// },6000)
+setTimeout(()=>{
+  element.innerText = 4;
+// },7000)
+setTimeout(()=>{
+  element.innerText = 3;
+// },8000)
+setTimeout(()=>{
+  element.innerText = 2;
+// },9000)
+setTimeout(()=>{
+  element.innerText = 1;
+// },10000)
+setTimeout(()=>{
+  element.innerText = "Happy New Year";
+// },11000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
